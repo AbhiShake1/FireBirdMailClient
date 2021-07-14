8f61b8ac5a0b0e174ddef8bbb19b26f3976ec109
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-07-14 19:22:01 +0545

    convert class to record and minor refactor

--- src/com/abhi/model/SizeInteger.java
+++ src/com/abhi/model/SizeInteger.java
@@ -1,35 +1,23 @@
 package com.abhi.model;
 
-public class SizeInteger implements Comparable<SizeInteger>{
-
-    private int size;
-
-    public SizeInteger(int size) {
-        this.size = size;
-    }
+public record SizeInteger(int size) implements Comparable<SizeInteger> {
 
     @Override
     public String toString() {
-        if (size <= 0){
+        if (size <= 0) {
             return "0";
-        }else if(size < 1024){
-            return size+"8";
-        }else if(size < 1048576){
-            return (size/1024)+" kB";
-        }else {
-            return (size/1048676)+" MB";
+        } else if (size < 1024) {
+            return size + "8";
+        } else if (size < 1048576) {
+            return (size / 1024) + " kB";
+        } else {
+            return (size / 1048676) + " MB";
         }
     }
 
     //sort by size
     @Override
     public int compareTo(SizeInteger sizeInteger) { //0, 1 or -1
-        if(size > sizeInteger.size){
-            return 1;
-        }else if(sizeInteger.size > size){
-            return -1;
-        }else {
-            return 0;
-        }
+        return Integer.compare(size, sizeInteger.size);
     }
 }
