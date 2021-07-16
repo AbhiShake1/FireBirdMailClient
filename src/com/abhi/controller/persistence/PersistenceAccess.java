1f218621560c4109cccdd62908b8e1edcfd28f2c
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-07-17 00:51:50 +0545

    encode password before saving for better security

--- src/com/abhi/controller/persistence/PersistenceAccess.java
+++ src/com/abhi/controller/persistence/PersistenceAccess.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersistenceAccess {
+    
+    private final Encoder encoder = new Encoder();
 
     //this is where serialized object is accessed from
     //random extension so that it will be hidden in the location
@@ -16,9 +18,18 @@ public class PersistenceAccess {
             FileInputStream inputStream = new FileInputStream(DIRECTORY_VALID_ACCOUNTS);
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
             List<ValidAccount> persistentList = (List<ValidAccount>) objectInputStream.readObject();
+            decodePasswords(persistentList);
             resultList.addAll(persistentList);
         }catch (Exception ignored){}
         return resultList;
+    }
+
+    private void decodePasswords(List<ValidAccount> persistentList) {
+        persistentList.forEach(v->v.setPassword(encoder.decode(v.getPassword())));
+    }
+
+    private void encodePasswords(List<ValidAccount> persistentList) {
+        persistentList.forEach(v->v.setPassword(encoder.encode(v.getPassword())));
     }
 
     public void saveAccounts(List<ValidAccount> validAccounts){//invoke when app stops
@@ -26,6 +37,7 @@ public class PersistenceAccess {
         try{
             FileOutputStream outputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            encodePasswords(validAccounts);
             //save
             objectOutputStream.writeObject(validAccounts);
             outputStream.close();
